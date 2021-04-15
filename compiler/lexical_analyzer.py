f0ca614216733afd65b3a1421d3f5d335ba65b23
InKyeongBae <hwang0809@cau.ac.kr>
2021-04-15 23:12:04 +0900

    [FIX] 모든 에러 확인 및 코드 정리
    ^ 처리 빼고 모두 끝

--- compiler/lexical_analyzer.py
+++ compiler/lexical_analyzer.py
@@ -24,13 +24,9 @@ error_line = 0
 is_error = 0
 
 # 일단 input 내용 그대로 out으로 나오도록
-for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
-    #print("###############"
-     #     "########################")
+for inputStr in inputline:
     table = []
-
     inputline = inputStr
-    #print(inputline)
     text1 = deque(inputline)
     textState = 0
 
@@ -44,8 +40,7 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
     else :
         while len(text1) > 0:
             error = []
-            if text1[textState] in symbols and text1[textState] not in WHITESPACE:
-
+            if text1[textState] in symbols :
                 for i in range(len(MERGED1)):
                     if i == 0:
                         while textState < len(text1) and MERGED1[i].wscheckIng(text1[textState]) == 1:
@@ -54,7 +49,6 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
                                 textState += 1
                             else:
                                 break
-                            ##
 
                     else:
                         while textState < len(text1) and MERGED1[i].checkIng(text1[textState]) == 1:
@@ -79,10 +73,8 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
 
                     elif MERGED1[i].isAccepted() == False and i == (len(MERGED1) - 1):
                         MERGED1[i].clear()
-                        print(text1,"!!!!!!!!!!!!!!")
                         while(len(text1) > 0) :
                             text1.pop()
-                        print("EMPTY", text1)
                         error_num = error_num + 1
                         file_out.close()
                         file_out = open(f"{filename.replace('.java', '')}.out", 'w')
@@ -102,7 +94,6 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
 
                     else:
                         MERGED1[i].clear()
-
                         i += 1
                         textState = 0
 
@@ -133,18 +124,42 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
                                 table.append(['TYPE', subStr])
                             textState = 0
                             MERGED2[i].clear()
+                            break
+
+                        elif MERGED2[i].isAccepted() == False and i == (len(MERGED2) - 1):
+                            MERGED2[i].clear()
+                            while (len(text1) > 0):
+                                text1.pop()
+                            error_num = error_num + 1
+                            file_out.close()
+                            file_out = open(f"{filename.replace('.java', '')}.out", 'w')
+                            printStr = "ERROR: is not match type, line:" + str(error_line)
+
+                            filewrite(file_out, printStr)
+                            filewrite(file_out, '\n')
+                            is_error = 1
+                            for index in range(error_num):
+                                error.append(" ")
+                            error[error_num - 1] = "^"
+                            filewrite(file_out, inputline.rstrip('\n') + '\n')
+                            filewrite(file_out, ''.join(error) + '\n')
+
+                            i += 1
+                            textState = 0
+
                         else:
                             MERGED2[i].clear()
                             i += 1
                             textState = 0
+
+                    # 마지막 글자가 아니면
                     else:
-                        if MERGED2[i].isAccepted() and (
-                                text1[textState] == ' ' or text1[textState] != '_' or text1[textState] not in LETTER or
-                                text1[textState] not in DIGIT) and i != len(MERGED2) - 1:
+                        if MERGED2[i].isAccepted() and (text1[textState] != '_' and text1[textState] not in LETTER and text1[textState] not in DIGIT) and i != len(MERGED2) - 1:
                             subStr = ""
                             for _ in range(textState):
                                 subStr += text1.popleft()
                                 error_num = error_num + 1
+
                             if i >= 0 and i < 2:
                                 table.append(['BOOLEAN', subStr])
                             elif i >= 2 and i < 7:
@@ -153,54 +168,68 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
                                 table.append(['TYPE', subStr])
                             textState = 0
                             MERGED2[i].clear()
+                            break
 
                         elif MERGED2[i].isAccepted() and i == len(MERGED2) - 1:
                             subStr = ""
                             for _ in range(textState):
                                 subStr += text1.popleft()
                                 error_num = error_num + 1
+
                             table.append([MERGED2[i].acceptedToken(), subStr])
                             textState = 0
                             MERGED2[i].clear()
+                            break
+
+                        elif MERGED2[i].isAccepted() == False and i == (len(MERGED2) - 1):
+                            MERGED2[i].clear()
+                            while (len(text1) > 0):
+                                text1.pop()
+                            error_num = error_num + 1
+                            file_out.close()
+                            file_out = open(f"{filename.replace('.java', '')}.out", 'w')
+                            printStr = "ERROR: is not match type, line:" + str(error_line)
+
+                            filewrite(file_out, printStr)
+                            filewrite(file_out, '\n')
+                            is_error = 1
+                            for index in range(error_num):
+                                error.append(" ")
+                            error[error_num - 1] = "^"
+                            filewrite(file_out, inputline.rstrip('\n') + '\n')
+                            filewrite(file_out, ''.join(error) + '\n')
+
+                            i += 1
+                            textState = 0
+
                         else:
                             MERGED2[i].clear()
                             i += 1
                             textState = 0
 
 
-            # 3) 숫자가 들어올 때
             elif text1[textState] in OPERATOR or text1[0] in DIGIT:
                 idx = 0
                 if table and table[-1][0] == "IDENTIFIER" and text1[textState] == "-":  # MINUS PROBLEM
                     idx = 1
                 if table and table[-1][0] == "OPERATOR" and text1[textState] == "-":  # MINUS PROBLEM
                     idx = 0
+
                 for i in range(idx, len(MERGED3)):
                     while textState < len(text1) and MERGED3[i].checkIng(text1[textState]) == 1:
                         MERGED3[i].nextState(text1[textState])
                         if MERGED3[i].isIng == 1:
                             textState += 1
-                        else:  # accaept이 안됨
+                        else:
                             break
 
-                    # MERGED를 다 돈게 아닌 상태에서 넘어감.
                     if MERGED3[i].isAccepted():
-
                         subStr = ""
                         text1copy = text1
-                        # if textState == 0: #MINUS PROBLEM -1 #for operator "-" ex) 1-1
-                        #    subStr += text1.popleft()
 
                         for _ in range(textState):
                             subStr += text1.popleft()  # 1
                             error_num = error_num + 1
-
-                        # print(text1)
-                        # if text1[0] == "-": #MINUS PROBLEM -2 #
-                        #    if table[-1][0] == "IDENTIFIER":
-                        #        table.append(["OPERATOR", subStr[0]])
-                        #        table.append([MERGED3[i].acceptedToken(), subStr[1:]])
-                        # else:
 
                         table.append([MERGED3[i].acceptedToken(), subStr])
                         textState = 0
@@ -210,6 +239,28 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
                             i = 1
                         else:
                             i = 0
+                        break
+
+                    elif MERGED3[i].isAccepted() == False and i == (len(MERGED3) - 1):
+                        MERGED3[i].clear()
+                        while (len(text1) > 0):
+                            text1.pop()
+                        error_num = error_num + 1
+                        file_out.close()
+                        file_out = open(f"{filename.replace('.java', '')}.out", 'w')
+                        printStr = "ERROR: is not match type, line:" + str(error_line)
+
+                        filewrite(file_out, printStr)
+                        filewrite(file_out, '\n')
+                        is_error = 1
+                        for index in range(error_num):
+                            error.append(" ")
+                        error[error_num - 1] = "^"
+                        filewrite(file_out, inputline.rstrip('\n') + '\n')
+                        filewrite(file_out, ''.join(error) + '\n')
+
+                        i += 1
+                        textState = 0
 
                     else:
                         MERGED3[i].clear()
@@ -218,7 +269,7 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
 
 
             elif text1[textState] in WHITESPACE:
-                print("~^~^~^", text1)
+
                 for i in range(len(MERGED4)):
                     while textState < len(text1) and MERGED4[i].wscheckIng(text1[textState]) == 1:
                         MERGED4[i].nextState(text1[textState])
@@ -233,9 +284,10 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
                         for _ in range(textState):
                             subStr += text1.popleft()
                             error_num = error_num + 1
-                        # table.append([MERGED4[i].acceptedToken(), subStr])
                         textState = 0
                         MERGED4[i].clear()
+                        break
+
                     else:
                         MERGED4[i].clear()
                         i += 1
@@ -265,7 +317,6 @@ for inputStr in inputline:  ##아래 있는 inputline이랑 왜 다르지?
 
         if is_error == 0 :
             for i in range(len(table)):
-                print("table:", table[i])
                 filewrite(file_out, str(table[i]))
                 filewrite(file_out, "\n")
 
