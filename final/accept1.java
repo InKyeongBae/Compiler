fe9343f7c7a88c95847c0d68f73ae63295c3b854
InKyeongBae <hwang0809@cau.ac.kr>
2021-06-03 01:08:16 +0900

    [UPDATE] accept1.java & output

--- final/accept1.java
+++ final/accept1.java
@@ -2,8 +2,11 @@ int main(String hello) {
     int num1 = 0;
     char char1 = 'a';
     boolean bool1 = true;
-    while(true == false){
+    while(true == false) {
         String h = "Hello";
+    }
+    if(true < ( false < (true < false) ) ) {
+        int i = 5;
     }
     return h + hello;
 }
